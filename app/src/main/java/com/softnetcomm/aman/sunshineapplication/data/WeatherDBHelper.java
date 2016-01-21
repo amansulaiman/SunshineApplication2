f3a9c2d4902e2613ff754e513eb64b0d40e186c1
Abdulrahman <amansulaiman92@gmail.com>
2016-01-21 13:26:47 +0100

    nee chnages

--- app/src/main/java/com/softnetcomm/aman/sunshineapplication/data/WeatherDBHelper.java
+++ app/src/main/java/com/softnetcomm/aman/sunshineapplication/data/WeatherDBHelper.java
@@ -17,7 +17,7 @@ public class WeatherDBHelper extends SQLiteOpenHelper {
     public WeatherDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
-
+//Weather contract
     @Override
     public void onCreate(SQLiteDatabase db) {
 
@@ -26,6 +26,7 @@ public class WeatherDBHelper extends SQLiteOpenHelper {
                         WeatherEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
                         WeatherEntry.COLUMN_LOCATION_KEY + " INTEGER NOT NULL, " +
                         WeatherEntry.COLUMN_DATETEXT + "";
+
     }
 
     @Override
